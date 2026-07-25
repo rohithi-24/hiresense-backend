88b425b0b599207f1ff783510de8c1b5b9c315d6
Rohithi <rpilla3@gitam.in>
2026-07-26 01:47:04 +0530

    Allow Netlify frontend domain in CORS origins

--- app/main.py
+++ app/main.py
@@ -12,7 +12,11 @@ app = FastAPI(title="HireSense API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://hirsense.netlify.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
